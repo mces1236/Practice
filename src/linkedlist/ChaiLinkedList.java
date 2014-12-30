2e0b9ce985e5ba75ba66ea0749c137502f9fc1d8
chaitanya <chaitanya@veooz.com>
2014-12-30 18:53:46 +0530

    reverse and finding middle element in linked list

--- src/linkedlist/ChaiLinkedList.java
+++ src/linkedlist/ChaiLinkedList.java
@@ -5,9 +5,9 @@ import basic.ChaiSingleNode;
 
 
 public class ChaiLinkedList<T> {
-	
+
 	public ChaiSingleNode<T> head;
-	
+
 	public void add(T data){
 		ChaiSingleNode<T> node = new ChaiSingleNode<T>(data);
 
@@ -22,28 +22,81 @@ public class ChaiLinkedList<T> {
 			temp.next = node;
 		}
 	}
-	
+
 	public ChaiIterator<T> iterator(){
 		ChaiIterator<T> iterator = new ChaiIterator<T>(head);
 		return iterator;
 	}
-	
+
 	public T nthNodeFromEnd(int n){
 		ChaiSingleNode<T> temp, nthNode;
 		temp = nthNode = head;
-		
+
 		int size = size();
 		if(n > size) return null;
-		
+
 		for(int i = 0; i < n; i++){
 			nthNode = nthNode.next;
 		}
-		
+
 		for(;nthNode != null; nthNode = nthNode.next){
 			temp = temp.next;
 		}
-		
+
 		return temp.data;
+	}
+
+	public void reverse(){
+		ChaiSingleNode<T> cur = head, temp = null, pre = null;
+
+		while(cur != null){
+			temp = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = temp;
+		}
+
+		head = pre;
+	}
+
+	public ChaiSingleNode<T> reverse(ChaiSingleNode<T> cur){
+		ChaiSingleNode<T> temp = null, pre = null;
+
+		while(cur != null){
+			temp = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = temp;
+		}
+
+		return pre;
+	}
+
+	public ChaiSingleNode<T> getMiddle(){
+		ChaiSingleNode<T> singleJump = head, doubleJump = head;
+
+		while(doubleJump != null){
+			singleJump = singleJump.next;
+			doubleJump = doubleJump.next;
+
+			if(doubleJump != null){
+				doubleJump = doubleJump.next;
+			}
+		}
+
+		return singleJump;
+	}
+
+	public boolean isPalindrome(){
+		ChaiSingleNode<T> middle = getMiddle(), temp = head;
+		middle = reverse(middle);
+
+		for(; (middle != null && temp.data == middle.data); temp = temp.next, middle = middle.next);
+
+		if(middle == null)
+			return true;
+
+		return false;
 	}
 
 	public int size() {
@@ -53,12 +106,20 @@ public class ChaiLinkedList<T> {
 		}
 		return size;
 	}
-	
+
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		ChaiIterator<T> iterable = iterator();
 		for(T data = iterable.next(); data != null; data = iterable.next()){
 			sb.append(data);
+		}
+		return sb.toString();
+	}
+
+	public String toString(ChaiSingleNode<T> node){
+		StringBuilder sb = new StringBuilder();
+		for(ChaiSingleNode<T> temp = node; temp != null; temp = temp.next){
+			sb.append(temp.data);
 		}
 		return sb.toString();
 	}
--- src/test/ChaiLinkedListTest.java
+++ src/test/ChaiLinkedListTest.java
@@ -1,16 +1,22 @@
 package test;
 
+import basic.ChaiSingleNode;
 import linkedlist.ChaiLinkedList;
 
 public class ChaiLinkedListTest {
 	public static void main(String[] args) {
 		ChaiLinkedList<Integer> list = new ChaiLinkedList<Integer>();
 
-		for(int i = 0; i < 6; i++){
+		for(int i = 1; i < 3; i++){
 			list.add(i);
 		}
-		System.out.println(list);
-		System.out.println(list.size());
-		System.out.println(list.nthNodeFromEnd(1));
+		for(int i = 2; i > 0; i--){
+			list.add(i);
+		}
+		
+		ChaiSingleNode<Integer> middle = list.getMiddle();
+		System.out.println(middle.data);
+		middle = list.reverse(middle);
+		System.out.println(list.toString(middle));
 	}
 }
