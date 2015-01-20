833dcbc0ee77812bfa5b66c6bb8074c6251ba0e9
chaitanya <chaitanya@veooz.com>
2015-01-20 18:53:10 +0530

    reverse every alternate k nodes

--- src/linkedlist/ChaiLinkedList.java
+++ src/linkedlist/ChaiLinkedList.java
@@ -20,7 +20,7 @@ public class ChaiLinkedList<T> {
 			tail = node;
 		}
 	}
-	
+
 	public ChaiIterator<T> iterator(){
 		ChaiIterator<T> iterator = new ChaiIterator<T>(head);
 		return iterator;
@@ -43,46 +43,46 @@ public class ChaiLinkedList<T> {
 
 		return temp.data;
 	}
-	
+
 	public void pairWiseSwapUsing3Pointers(){
-		
+
 	}
-	
+
 	public void pairWiseSwapUsing2Pointers(){
 		ChaiSingleNode<T> current = head;
-		
+
 		if(current != null && current.next != null){
 			head = current.next;
-			
+
 			while(current != null && current.next != null){
 				ChaiSingleNode<T> next = current.next;
 				current.next = next.next;
 				next.next = current;
 				next = current.next;
-				
+
 				if(next != null && next.next != null)
 					current.next = next.next;
 				current = next;
 			}
 		}
 	}
-	
+
 	public void deleteAlternateNodes(){
 		ChaiSingleNode<T> temp = head;
-		
+
 		while(temp != null && temp.next != null){
 			temp.next = temp.next.next;
 			temp = temp.next;
 		}
 	}
-	
+
 	public void splitAlternateNodes(ChaiLinkedList<T> list1, ChaiLinkedList<T> list2){
 		ChaiSingleNode<T> temp = head;
-		
+
 		while(temp != null){
 			list1.add(temp.data);
 			temp = temp.next;
-			
+
 			if(temp != null){
 				list2.add(temp.data);
 				temp = temp.next;
@@ -102,32 +102,6 @@ public class ChaiLinkedList<T> {
 
 		head = pre;
 	}
-	
-	public void interchangeFirstandLast(){
-		
-		if(head.next != null){
-			ChaiSingleNode<T> temp = head;
-			
-			while(temp.next.next != null){
-				temp = temp.next;
-			}
-			
-			temp.next.next = head;
-			head = temp.next;
-			temp.next = null;
-		}
-	}
-	
-	private void printReverse(ChaiSingleNode<T> temp){
-		if(temp != null){
-			printReverse(temp.next);
-			System.out.println(temp.data);
-		}
-	}
-	
-	public void printReverse(){
-		printReverse(head);
-	}
 
 	public ChaiSingleNode<T> reverse(ChaiSingleNode<T> cur){
 		ChaiSingleNode<T> temp = null, pre = null;
@@ -140,6 +114,104 @@ public class ChaiLinkedList<T> {
 		}
 
 		return pre;
+	}
+
+	public void reverseEveryKNodes(int k){
+		ChaiSingleNode<T> cur, pre = null, next, loopFirst, loopLast = null;
+		boolean changeHead = true;
+		int i;
+		cur = head;
+
+		while(cur != null){
+			i = 0;
+			loopFirst = cur;
+			while(cur != null && i < k){
+				next = cur.next;
+				cur.next = pre;
+				pre = cur;
+				cur = next;
+				i++;
+			}
+
+			if(changeHead){
+				changeHead = false;
+				head = pre;
+			}
+
+			if(loopLast != null)
+				loopLast.next = pre;
+
+			loopLast = loopFirst;
+			pre = null;
+			loopFirst.next = cur;
+		}
+	}
+
+	public void reverseEveryAlternateKNodes(int k){
+		ChaiSingleNode<T> cur, pre = null, next, loopFirst = null;
+		boolean changeHead = true, isEvenLoop = false;
+		int i;
+		cur = head;
+
+		while(cur != null){
+			i = 0;
+			
+			if(isEvenLoop){
+				loopFirst = cur;
+			}
+			
+			while(cur != null && i < k){
+				if(isEvenLoop){
+					cur = cur.next;
+					i++;
+				}
+				else{
+					next = cur.next;
+					cur.next = pre;
+					pre = cur;
+					cur = next;
+					i++;
+				}
+			}
+
+			if(changeHead){
+				changeHead = false;
+				head = pre;
+			}
+
+			if(isEvenLoop){
+				pre = null;
+				loopFirst.next = cur;
+			}
+
+			isEvenLoop = true;
+		}
+	}
+
+	public void interchangeFirstandLast(){
+
+		if(head.next != null){
+			ChaiSingleNode<T> temp = head;
+
+			while(temp.next.next != null){
+				temp = temp.next;
+			}
+
+			temp.next.next = head;
+			head = temp.next;
+			temp.next = null;
+		}
+	}
+
+	private void printReverse(ChaiSingleNode<T> temp){
+		if(temp != null){
+			printReverse(temp.next);
+			System.out.println(temp.data);
+		}
+	}
+
+	public void printReverse(){
+		printReverse(head);
 	}
 
 	public ChaiSingleNode<T> getMiddle(){
@@ -190,7 +262,7 @@ public class ChaiLinkedList<T> {
 		}
 		return sb.toString();
 	}
-	
+
 	public String toString(ChaiSingleNode<T> node){
 		StringBuilder sb = new StringBuilder();
 		for(ChaiSingleNode<T> temp = node; temp != null; temp = temp.next){
--- src/test/ChaiLinkedListTest.java
+++ src/test/ChaiLinkedListTest.java
@@ -1,27 +1,28 @@
 package test;
 
 import linkedlist.ChaiLinkedList;
-import linkedlist.SortedChaiLinkedList;
 
 
 public class ChaiLinkedListTest {
 	public static void main(String[] args) {
 		ChaiLinkedList<Double> list = new ChaiLinkedList<Double>();
 		
-		for(double d = 2.0; d < 10.0; d=d+2.0){
+		for(double d = 1.0; d < 10.0; d++){
 			list.add(d);
 		}
 		
 		System.out.println(list);
+		list.reverseEveryAlternateKNodes(3);
+		System.out.println(list);
 		
-		ChaiLinkedList<Double> list1 = new ChaiLinkedList<Double>();
+//		ChaiLinkedList<Double> list1 = new ChaiLinkedList<Double>();
+//		
+//		for(double d = 1.0; d < 10.0;  d=d+2.0){
+//			list1.add(d);
+//		}
 		
-		for(double d = 1.0; d < 10.0;  d=d+2.0){
-			list1.add(d);
-		}
+//		System.out.println(list1);
 		
-		System.out.println(list1);
-		
-		System.out.println(SortedChaiLinkedList.mergeTwoSortedLists(list, list1));
+//		System.out.println(SortedChaiLinkedList.mergeTwoSortedLists(list, list1));
 	}
 }
