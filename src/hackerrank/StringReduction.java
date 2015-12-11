117634c11d533631efc1abb7806fb7e0e46c65c7
Chaitanya <chaitanyaeswar.chaitanya@gmail.com>
2015-12-11 22:46:50 +0530

    hr 1 str reduction

--- src/hackerrank/StringReduction.java
+++ src/hackerrank/StringReduction.java
@@ -2,8 +2,6 @@ package hackerrank;
 
 import java.util.Scanner;
 
-import com.sun.org.apache.xml.internal.security.keys.storage.StorageResolverException;
-
 public class StringReduction {
 
 	public static void main(String[] args) {
@@ -11,33 +9,33 @@ public class StringReduction {
 		Scanner sc = new Scanner(System.in);
 
 		int t = sc.nextInt();
+		sc.nextLine();
 
 		int[] ops = new int[t];
 		for(int i = 0; i < t; i++){
 			StringBuffer str = new StringBuffer(sc.nextLine());
 
-			int strlen = 0;
-			for(int j = 0; i < (str.length()-1); j++) {
-				char fir = str.charAt(j);
-				char sec = str.charAt(j+1);
-				int diff = fir - sec;
-
-				switch(diff) {
-				case 0 : strlen++; break;
-				case 1 : 
-					if((fir == 'a' && sec == 'b') || (fir == 'b' && sec == 'a')) {
-						str.insert((j+1), 'c');
-					}
-					else if((fir == 'b' && sec == 'c') || (fir == 'c' && sec == 'b')) {
-						str.insert((j+1), 'a');
-					}
-					break;
-				case 2 :
-					str.insert((j+1), 'b');
-					break;
+			int strlen = str.length(), cur = (strlen - 1), toDel = 0, sum = 'a' + 'b' + 'c';
+			
+			while(cur > 0) {
+				char curChar = str.charAt(cur);
+				char preChar = str.charAt((cur-1));
+				
+				if (curChar == preChar) {
+					toDel++;
+					cur--;
+				}
+				else {
+					char toInsert = (char)(sum - (curChar + preChar));
+					str.setCharAt((cur-1), toInsert);
+					strlen--;
+					
+					if(toDel > 0) toDel--;
+					else cur--;
 				}
 			}
-			ops[i] = strlen+1;
+			
+			ops[i] = strlen;
 		}
 
 		sc.close();
