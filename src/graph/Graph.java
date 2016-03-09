4a813ab12d7506826f76b8ef8c952ccfcbce0b75
Chaitanya <chaitanya@veooz.com>
2016-03-09 18:20:19 +0530

    graph

---
+++ src/graph/Graph.java
@@ -0,0 +1,14 @@
+package graph;
+
+public class Graph {
+	Vertex[] vertices;
+	int rootIndex = -1;
+	
+	public Graph(int noOfVertices) {
+		vertices = new Vertex[noOfVertices];
+	}
+	
+	public void createLink(int from, int to) {
+		
+	}
+}
---
+++ src/graph/Vertex.java
@@ -0,0 +1,11 @@
+package graph;
+
+public class Vertex {
+	int data;
+	Vertex link;
+	
+	public Vertex(int value) {
+		data = value;
+		link = null;
+	}
+}
